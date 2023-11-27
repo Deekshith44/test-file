a871152dbac03034faadf12c89b583de4c94a1d0
unknown <dixithpoojary444@gmail.com>
2023-11-27 10:48:32 +0530

    hi

---
+++ app.js
@@ -0,0 +1,10 @@
+const animals = ["chitha", "tiger", "elephant", "cow"];
+
+//animals.push("dog", "cat", "rabit");
+
+//animals.pop();
+animals.shift();
+
+animals.unshift("me", "you", "everyone");
+
+console.log(animals);
